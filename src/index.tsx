5a4e0bbff7701024b7f7d38db0a8e8903508d9cf
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2023-10-26 16:14:43 +0700

    app niu

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <div>NIU</div>
   </React.StrictMode>
 );
 
