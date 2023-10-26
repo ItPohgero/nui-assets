48f131eb38302c47a171c5babf087742109bfe56
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2023-10-26 17:09:03 +0700

    upduated text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,8 @@
+import { useColor } from '../hooks'
 import { TextProps } from '../interface/TextProps'
 export function Text(props: TextProps): any {
-    const { value, color, size } = props
+    const { Slate } = useColor()
+    const { value, color = Slate[500], size = "md" } = props
     return (
         <p style={{ color, fontSize: size }}>{value}</p>
     )
--- src/interface/TextProps.ts
+++ src/interface/TextProps.ts
@@ -2,6 +2,6 @@ import { ColorProps } from "./ColorProps"
 
 export interface TextProps {
     value: string
-    size: 'xxs' | 'xs' | 'md' | 'lg' | 'xl' | "xxl"
-    color: string
+    size?: 'xxs' | 'xs' | 'md' | 'lg' | 'xl' | "xxl"
+    color?: string
 }
