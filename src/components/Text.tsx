6130d2187c2654032c0238fc6057ce35657a0e7f
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2023-10-26 11:07:53 +0700

    text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,10 +1,8 @@
-import useColor from '../hooks/useColor'
 import { TextProps } from '../interface/TextProps'
 const Text = (props: TextProps) => {
     const { value, color, size } = props
-    const { Amber } = useColor()
     return (
-        <div style={{ color, fontSize: size }}>{value}</div>
+        <p style={{ color, fontSize: size }}>{value}</p>
     )
 }
 export default Text
