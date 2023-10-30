1d0a0b2079915647fccaec44e1a258f61de637f9
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2023-10-30 10:40:28 +0700

    use search

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react';
 import { Table } from '../components';
 import { useTable as useTableCore } from '../hooks';
 import { ColumnsTable, DataTable, SummaryTable } from '../data/table';
+import { useSearch } from '../hooks/useSearch';
 
 const meta = {
     title: 'NIU/Table/Core',
@@ -14,17 +16,32 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Example: Story = (args: any) => {
+
+    const [search, setSearch] = React.useState<string>('')
+    let DataFil = useSearch({
+        data: DataTable,
+        keys: ["name"],
+        min: 3,
+        search: search
+    })
+
     const useTableProps: any = useTableCore({
         summary: SummaryTable,
         options: {
             bordered: true,
             pagination: true,
         },
-        data: DataTable,
+        data: DataFil,
         columns: ColumnsTable,
     });
 
-    return <Table widthNumber {...args} useTableProps={useTableProps} OnChangePagination={() => {}} />;
+
+    return (
+        <div>
+            <input className='bg-slate-200 py-1 mb-6 border-none focus:border-none active:border-none' onChange={(e: any) => setSearch(e?.target?.value)} />
+            <Table widthNumber {...args} useTableProps={useTableProps} OnChangePagination={() => { }} />
+        </div>
+    );
 };
 
 Example.args = {};
