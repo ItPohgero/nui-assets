72c858e181739897c051c014380a3d4ca024c4b2
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2023-11-15 11:13:27 +0700

    fix: gap

--- src/hooks/TestHook.tsx
+++ src/hooks/TestHook.tsx
@@ -24,7 +24,7 @@ export function TestHook(): any {
             key: '2',
             label: 'useColor',
             children: (
-              <div className="grid grid-cols-6">
+              <div className="grid grid-cols-6 gap-4">
                 <div style={{ backgroundColor: Rose[500] }} className="text-white flex justify-center items-center rounded-lg h-20 w-full">Rose.500</div>
                 <div style={{ backgroundColor: Blue[500] }} className="text-white flex justify-center items-center rounded-lg h-20 w-full">Blue.500</div>
               </div>
